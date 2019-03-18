131d5cd106368c8d28623b0cf6762dbef51282ba
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-03-18 01:39:18 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+var logger = require('morgan');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/WellnessTracker";
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(logger("dev"));
+
+mongoose.connect(MONGODB_URI);
+
+const db = require(path.join(__dirname, "./models/"));
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+require('./routes/dataRoutes')(app, db);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`)
+})
