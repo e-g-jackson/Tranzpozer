9108117246d7f5139c3e95beca2bb68a85901a89
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-03-19 18:18:16 -0500

    react-router-dom Links added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 class Navbar extends React.Component{
     render(){
         return(
             <div className = "navbar navbar-expand-lg navbar-dark bg-dark">
-                <a href = "/">
+                <Link to = "/">
                     <button className = "btn btn-sm btn-warning mr-3">Home</button>
-                </a>
-                <a href = "/guitar">
+                </Link>
+                <Link to = "/guitar">
                     <button className = "btn btn-sm btn-success mr-3">Noteizer</button>
-                </a>
+                </Link>
             </div>
         )
     }
