4b8c692579f59c17b9e713f8249a358de4ffb9f3
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-03-21 16:54:41 -0500

    commented out homepage, due to reload-error

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,19 @@ class App extends Component {
       <Router>
         <Navbar />
         <Route exact path = "/" render = {() =>{
+          // return(
+          //   <div className = "container text-center">
+          //     <h1>The Tranzpozer App!</h1>
+          //     <br />
+          //     <p>
+          //       The app to make all the guitar headaches go away...
+          //     </p>
+          //   </div>
+          // )
           return(
-            <div className = "container text-center">
-              <h1>The Tranzpozer App!</h1>
-              <br />
-              <p>
-                The app to make all the guitar headaches go away...
-              </p>
+            <div>
+              <h1 className = "mt-5 ml-5 pl-5"><strong>Guitar:</strong></h1>
+              <Searcher />
             </div>
           )
         }} />
